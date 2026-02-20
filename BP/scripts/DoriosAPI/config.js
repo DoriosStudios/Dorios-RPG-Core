6fb1bf90aceb6c02e734571b03d416bcbecc7d42
Kauziin_ <74920731+Kauziin@users.noreply.github.com>
2026-02-20 11:32:58 -0300

    refactor(bp/scripts): modularize doriosapi bootstrap

---
+++ BP/scripts/DoriosAPI/config.js
@@ -0,0 +1,13 @@
+export const addonData = {
+    name: "Dorios RPG Core",
+    author: "Dorios Studios",
+    identifier: "dorios_rpg_core",
+    version: "1.0.1"
+}
+
+import './API.js'
+import './dependencyChecker.js'
+import './blockClass.js'
+import './playerClass.js'
+import './itemStackClass.js'
+import './entityClass.js'
